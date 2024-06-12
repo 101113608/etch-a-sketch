15ba584f2abe437263f3a9f60be7a3f358cb09af
101113608 <48575130+101113608@users.noreply.github.com>
2024-06-12 13:24:09 +1000

    Add arrays storing the row and column of divs
    To order to store the 16x16 grid of div elements, a multi-dimensional
    array was made using an array for rows and another array for columns.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+const ROW_LENGTH = 16;
+const COLUMN_LENGTH = 16;
 const container = document.querySelector(".container");
+
+let grids = [];
+
+for (let row = 0; row < ROW_LENGTH; row++) {
+    // Store a new column array for each row iteration
+    grids[row] = new Array()
+
+    for (let column = 0; column < COLUMN_LENGTH; column++) {;
+        grids[row][column] = document.createElement("div");
+        container.appendChild(grids[row][column]);
+    }
+}

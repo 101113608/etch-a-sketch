dcd66f22cc04603f3f4297c233a612e721246ac5
101113608 <48575130+101113608@users.noreply.github.com>
2024-06-17 08:44:07 +1000

    Refactor grid array and grid-related functions
    With how the grid array is currently structured and stored in JS, the
    CSS is not able to evenly distribute the given size of squares on both
    sides. In order to achieve this, another set of div elements that act as
    the 'columns' of the grid need to be added, so that the container will
    have 'x' amount of columns, with each column having 'x' amount of
    'rows', where 'x' is the given size of the grid.
    
    As a result, the way div elements were stored when forming the grid had
    to be changed to accommodate for both 'column' and 'row' divs, which
    resulted in using two arrays instead of one. Alongside this, functions
    related to manipulating the grid (i.e., dissolving and creating a grid)
    were also refactored to accommodate for this change.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 const container = document.querySelector(".container");
 const gridBtn = document.querySelector("#gridBtn");
 
-let grid = [];
+let gridColumn = [];
+let gridRow = [];
 let squaresPerSide = 16;
 
-generateGrid(grid, squaresPerSide);
+generateGrid(gridColumn, gridRow, squaresPerSide);
 
 container.addEventListener('mouseover', (e) => {
-    if (e.target.classList.contains("square")) {
+    if (e.target.classList.contains("row")) {
         e.target.style["background-color"] = "lightgrey";
     }
 })
@@ -16,8 +17,8 @@ gridBtn.addEventListener('click', (e) => {
     let input = +prompt("Enter number of squares per side (maximum 100 per side)");
 
     if (checkSize(input)) {
-        removeGrid(grid);
-        generateGrid(grid, input);
+        removeGrid(gridColumn, gridRow);
+        generateGrid(gridColumn, gridRow, input);
     }
 })
 
@@ -28,32 +29,34 @@ function checkSize(input) {
     return false;
 }
 
-function generateGrid(grid, size) {
+function generateGrid(gridColumn, gridRow, size) {
     squaresPerSide = size;
 
-    for (let row = 0; row < squaresPerSide; row++) {
-        // Store a new column array for each row iteration
-        grid[row] = new Array();
+    for (let column = 0; column < squaresPerSide; column++) {
+        gridColumn[column] = document.createElement("div");
+        gridColumn[column].setAttribute("class", "column");
 
-        for (let column = 0; column < squaresPerSide; column++) {
-            grid[row][column] = document.createElement("div");
-            grid[row][column].setAttribute("class", "square");
-            container.appendChild(grid[row][column]);
+        for (let row = 0; row < squaresPerSide; row++) {
+            gridRow[row] = document.createElement("div");
+            gridRow[row].setAttribute("class", "row");
+            gridColumn[column].appendChild(gridRow[row]);
+
         }
+        container.appendChild(gridColumn[column]);
     }
 }
 
-function removeGrid(grid) {
-    if (grid === undefined || grid.length === 0)
+function removeGrid(gridColumn, gridRow) {
+    if (gridColumn === undefined || gridColumn.length === 0 || gridRow === undefined || gridRow.length === 0)
         return;
 
-    for (let row = squaresPerSide - 1; row >= 0; row--) {
-        for (let column = squaresPerSide - 1; column >= 0; column--) {
-            container.removeChild(grid[row][column]);
-            grid[row].splice(column, 1);
+    for (let column = gridColumn.length - 1; column >= 0; column--) {
+        for (let row = gridRow.length - 1; row >= 0; row--) {
+            gridColumn[column].removeChild(gridRow[row]);
+            gridRow.splice(row, 1);
         }
-
-        grid.splice(row, 1);
+        container.removeChild(gridColumn[column]);
+        gridColumn.splice(column, 1);
     }
     squaresPerSide = 0;
 }

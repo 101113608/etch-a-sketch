f89c833e1c1d8579158763c5d3f4140ae9660acc
101113608 <48575130+101113608@users.noreply.github.com>
2024-06-17 11:46:31 +1000

    Change button sizing, code formatting

--- scripts.js
+++ scripts.js
@@ -62,5 +62,5 @@ function removeGrid(gridColumn, gridRow) {
 }
 
 function randomiseColour() {
-    return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
